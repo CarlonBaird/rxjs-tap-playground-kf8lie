dbea986dd59ade09bc0670f46417d63519083a27
CarlonBaird <carlyestrella@live.com>
2022-11-22 15:53:44 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,15 +1,19 @@
 import { of } from 'rxjs';
 import { filter, map, tap } from 'rxjs/operators';
 
-of(1, 7, 3, 6, 2, 8).pipe(
-  filter((value) => value > 3),
-  tap({
-    next: (value) => console.log('Next:', value),
-    error: (err) => console.log('Error:', err),
-    complete: () => console.log('Complete'),
-    subscribe: () => console.log('Subscribed'),
-    unsubscribe: () => console.log('Unsubscribed'),
-    finalize: () => console.log('Finalize')
-  }),
-  map((value) => value * 2)
-).subscribe();
+of(1, 7, 3, 6, 2, 8)
+  .pipe(
+    filter((value) => value > 3),
+    tap({
+      next: (value) => console.log('Next:', value),
+      error: (err) => console.log('Error:', err),
+      complete: () => console.log('Complete'),
+      subscribe: () => console.log('Subscribed'),
+      unsubscribe: () => console.log('Unsubscribed'),
+      finalize: () => console.log('Finalize'),
+    }),
+    map((value) => value * 2)
+  )
+  .subscribe();
+
+//Read: https://jaywoz.medium.com/information-is-king-tap-how-to-console-log-in-rxjs-7fc09db0ad5a
